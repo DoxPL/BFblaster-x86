a2092e4002b23af216ee5a624662a9cdbe762b1e
DoxPL <dox100@gmail.com>
2021-12-13 21:22:00 +0100

    Introduced program memory section

--- brainfck.asm
+++ brainfck.asm
@@ -2,10 +2,10 @@
 ; Author: Dominik G.
 
 section .data
-    SYNTAX_ERR_LB db 'Syntax error'
-    prog_buff times 30000 db 0
-    prog_ptr dw 0
-    symbol_index db 0
+    memory times 30000 db 0
+    program times 4096 db 0
+    mem_ptr dd 0
+    prog_ptr dd 0
 
 section .bss
     symbol resb 1
@@ -14,7 +14,7 @@ section .text
     global _start
 
 _start:
-    jmp run_instr
+    jmp load_prog
 
 _success:
     mov ebx, 0
@@ -28,76 +28,91 @@ _exit:
     mov eax, 1
     int 0x80
 
-run_instr:
-    mov byte [symbol], 0x0
+load_prog:
+    mov edx, dword [prog_ptr]
+    lea esi, [program + edx]
+    push esi
     call read_symbol
+    cmp byte [esi], 0x0
+    je run_prog
+    inc dword [prog_ptr] 
+    jmp load_prog
+
+run_prog:
+    mov dword [prog_ptr], 0
+    jmp next_instr
+
+next_instr:
+    mov edx, dword [prog_ptr]
+    lea esi, [program + edx]
+    inc dword [prog_ptr]
     ; symbol == '>'
-    cmp byte [symbol], 0x3e
+    cmp byte [esi], 0x3e
     je inc_ptr
     ; symbol == '<'
-    cmp byte [symbol], 0x3c
+    cmp byte [esi], 0x3c
     je dec_ptr
     ; symbol == '+'
-    cmp byte [symbol], 0x2b
+    cmp byte [esi], 0x2b
     je inc_val
     ; symbol == '-'
-    cmp byte [symbol], 0x2d
+    cmp byte [esi], 0x2d
     je dec_val
     ; symbol == '.'
-    cmp byte [symbol], 0x2e
+    cmp byte [esi], 0x2e
     je disp_char
     ; symbol == ','
-    cmp byte [symbol], 0x2c
+    cmp byte [esi], 0x2c
     je get_char
     ; symbol == '['
-    cmp byte [symbol], 0x5b
+    cmp byte [esi], 0x5b
     ; symbol == ']'
-    cmp byte [symbol], 0x5d
+    cmp byte [esi], 0x5d
     ; symbol == '0'
-    cmp byte [symbol], 0x0
+    cmp byte [esi], 0x0
     je _success
     jmp _failure
 
 inc_ptr:
-    inc dword [prog_ptr]
-    jmp run_instr
+    inc dword [mem_ptr]
+    jmp next_instr
 
 dec_ptr:
-    dec dword [prog_ptr]
-    jmp run_instr
+    dec dword [mem_ptr]
+    jmp next_instr
 
 inc_val:
-    mov edx, dword [prog_ptr]
-    lea esi, [prog_buff + edx]
+    mov edx, dword [mem_ptr]
+    lea esi, [memory + edx]
     inc byte [esi]
-    jmp run_instr
+    jmp next_instr
 
 dec_val:
-    mov edx, dword [prog_ptr]
-    lea esi, [prog_buff + edx]
+    mov edx, dword [mem_ptr]
+    lea esi, [memory + edx]
     dec byte [esi]
-    jmp run_instr
+    jmp next_instr
 
 disp_char:
-    mov edx, dword [prog_ptr]
-    lea esi, [prog_buff + edx]
+    mov edx, dword [mem_ptr]
+    lea esi, [memory + edx]
     push esi
     call put_char
-    jmp run_instr
+    jmp next_instr
 
 get_char:
     call read_symbol
-    mov edx, dword [prog_ptr]
-    lea esi, [prog_buff + edx]
+    mov edx, dword [mem_ptr]
+    lea esi, [memory + edx]
     mov dl, [symbol]
     mov byte [esi], dl
-    jmp run_instr
+    jmp next_instr
 
 read_symbol:
     push ebp
     mov ebp, esp
     mov edx, 1
-    mov ecx, symbol
+    mov ecx, [ebp + 8]
     mov ebx, 0
     mov eax, 3
     int 0x80
